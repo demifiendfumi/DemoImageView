8cc11f1e9b9b335853a73b9e1208f7a04613d178
LEE YAN YI <16022603@rp.edu.sg>
2018-04-24 11:38:41 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoimageview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.myapplicationdev.android.demoimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView ivDay2;
+    ImageView ivDay4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivDay2 = (ImageView)findViewById(R.id. imageView2);
+        ivDay2.setImageResource(R.drawable.day2);
+        ivDay4 = (ImageView)findViewById(R.id. imageView4);
+        ivDay4.setImageResource(R.drawable.day4);
+    }
+}
